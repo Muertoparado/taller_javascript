941948c27860a2782b041303a1874231644a0e4a
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-13 12:54:23 -0500

    feat: :memo: ejercicio 5

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-/* 3. Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente. */
+/* 5. Construir el algoritmo que lea por teclado dos números,
+si el primero es mayor al segundo informar su suma y
+diferencia, en caso contrario, informar el producto y la
+división del primero respecto al segundo. */
 
-let voltaje=0;
+let a=Number(prompt("digite el primer numero a: "));
+let b=Number(prompt("Digite el segundo numero b: "))
 
-let resistencia=Number(prompt("digite la resistencia total"));
-let corriente=Number(prompt("digite la corriente total"));
+if(a>>b){
+    console.log(`el #a ${a} mayor a #b ${b}`);
+    console.log(`suma numeros ${a+b} \n diferencia numeros ${a-b}`);
+    
+}else{
+    console.log(`el #b ${b} mayor a #a ${a}`);
+    console.log(`producto numeros ${a*b}\n division ${a/b}`)
 
-console.log(`el voltaje total circuito serie es: ${resistencia*corriente}V`);
+}
