0bb0cf57337ee8b15ac5695c9246e6e8d6614676
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-14 13:29:02 -0500

    feat(taller javascrip't): :poop: ejercicio 10

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ el sexo y la nota definitiva y halle al estudiante con la mayor
 nota y al estudiante con la menor nota y cuantos eran
 hombres y cuantos mujeres. */
 
-let estudiantes=[];
+ estudiantes=[];
 
-function estudiantes(nombre,sexo,nota){
+function estudiante(nombre,sexo,nota){
     this.nombre=nombre;
     this.sexo=sexo;
     this.nota=nota;
@@ -14,12 +14,30 @@ function estudiantes(nombre,sexo,nota){
  do{
     let nombre=prompt("digite el nombre: ");
     let sexo=prompt("digite el sexo (F o M): ");
-    let nota=parseFloat("digite la nota definitiva: ");
+    let nota=parseFloat(prompt("digite la nota definitiva: "));
+    let estudiant1=new estudiante(nombre,sexo,nota);
 
- }while(prompt("desea continuar"));
+    estudiantes.unshift(estudiant1);
+    console.log(estudiantes)
+
+ }while(confirm("desea continuar"));
+
 m=[];
 f=[];
+for (let i=0; i<estudiantes.legth;i++){
+   if(estudiantes[i].sexo=='f'){
+      contf+=1;
+      estf= new f (estudiante)
+       f.unshift(estf) 
+       console.log(`numero de mujeres`,contf)
+   }
+   if(estudiantes[i].sexo=='m'){
+      contm+=1;
+      estm= new m (estudiante)
+         m.unshift(estm)  
+        console.log(`numero de hombres`,contm)
+        console.log(m)
+   }
+}
 
- if(estudiantes.sexo=='f'){
-    m.unshift()
- }
+   
