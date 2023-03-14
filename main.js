594e308b00e0633d40d12746357680d6447709bb
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-14 07:10:16 -0500

    feat(taller javascrip't): :rocket: ejercicio 7

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-/* 4. Construir el algoritmo que solicite el nombre y edad de 3
-personas y determine el nombre de la persona con mayor edad. */
+/* 7. Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura. */
 
-let personas1={};
-personas1.nombre=prompt("digite el nombre persona 1 ");
-personas1.edad=Number(prompt("digite la edad persona 1"));
 
-let personas2={};
-personas2.nombre=prompt("digite el nombre persona 2 ");
-personas2.edad=Number(prompt("digite la edad persona 2"));
+let a=(prompt("digite el nombre del articulo: "));
+let b=Number(prompt("Digite el precio del articulo: "));
+let c=Number(prompt("Digite la cantidad de articulos: "))
 
-let personas3={};
-personas3.nombre=prompt("digite el nombre persona 3 ");
-personas3.edad=Number(prompt("digite la edad persona 3"));
 
-if(personas1.edad>>personas2.edad){
-    if(personas2.edad>>personas3.edad){
-        console.log(`persona de mayor edad llamada ${personas1[0]}`);
-    }else{
-        if(personas3.edad>>personas1.edad){
-            console.log(`persona de mayor edad llamada ${personas3[0]}`)
-        }
-    }
-}else{
-    if(personas1.edad>>personas3.edad){
-        console.log(`persona de mayor edad llamada ${personas2[0]}`);
-    }else{
-        if(personas3.edad>>personas2.edad){
-            console.log(`persona de mayor edad llamada ${personas3[0]}`)
+let precio=c*b;
 
-        }
-    }
-}
+console.log(`------factura------\n nombre ${a} \n precio a pagar ${precio}`)
