8e8d33bb19c2c0c124655c67d315d44d6f6acd47
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-14 08:47:16 -0500

    feat(taller javascrip't): :rocket: ejercicio 10

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-/* 8. Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo */
+/* 10. Desarrolle un programa cíclico que capture un dato
+numérico cada vez, y los vaya acumulando. El programa se
+detiene cuando el usuario digita un cero. El programa debe
+mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+VALOR Y MENOR VALOR */
+arr=[];
+do{
+   
+    op=Number(prompt("digite un nuero diferente de cero: "));
+    arr.unshift(op);
+    console.log(arr);
 
-
-let op=Number(prompt('1. para cuadrado \n 2.para triangulo '));
-
-switch (op){
-    case 1:
-        l=Number(prompt(`digite el lado del cuadrado`));
-        console.log(`perimetro ${l*4}`);
-        break;
-    case 2:
-        a=Number(prompt(`digite base del triangulo`));
-        b=Number(prompt(`altura triangulo`));
-        console.log(`area total triangulo ${a*b}`);
+}while(op!=0);
+let sum=0;
+let cont=0;
+for(const num of arr){
+    sum +=num;
+    cont+=1;
 }
+
+console.log(`sumatoria total ${sum}`);
+console.log(`numeros ingresados ${cont}`);
+console.log(`valor promedio ${sum/cont}`);
+console.log(`numero maximo `,Math.max(...arr));
+console.log(`numero minimo `,Math.min(...arr));
